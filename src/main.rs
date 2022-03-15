d3b9849509a1bbcf8caf10a5f440b05c3d765a09
danielnehrig <daniel.nehrig@dnehrig.com>
2022-03-15 03:10:30 +0100

    added docker compose and added mongodb
    Signed-off-by: danielnehrig <daniel.nehrig@dnehrig.com>

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,24 @@ pub mod auth {
     tonic::include_proto!("auth");
 }
 
+use std::sync::Arc;
+
 use auth::auth_server::{Auth, AuthServer};
+use futures::stream::StreamExt;
+use mongodb::options::ClientOptions;
+use serde::{Deserialize, Serialize};
 use tonic::{transport::Server, Response, Status};
 
-#[derive(Debug, Default)]
-struct AuthService;
+#[derive(Debug)]
+struct AuthService {
+    client: Arc<mongodb::Client>,
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+struct Credentials {
+    username: String,
+    password: String,
+}
 
 #[tonic::async_trait]
 impl Auth for AuthService {
@@ -16,9 +29,20 @@ impl Auth for AuthService {
     ) -> Result<Response<auth::Token>, Status> {
         println!("Got a request: {:?}", request);
 
+        let db = self.client.default_database().unwrap();
+        let col = db.collection::<Credentials>("users");
+
         let reply = auth::Token {
             auth: format!("Hello {}!", request.into_inner().username).into(),
         };
+
+        let data = col
+            .find(None, None)
+            .await
+            .unwrap()
+            .map(|e| e.unwrap())
+            .collect::<Vec<Credentials>>()
+            .await;
 
         Ok(Response::new(reply))
     }
@@ -33,13 +57,29 @@ impl Auth for AuthService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let greeter = AuthService::default();
+    let addr = "127.0.0.1:50051".parse()?;
+
+    let config = tonic_web::config().allow_all_origins();
+    // Parse a connection string into an options struct.
+    let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
+
+    // Manually set an option.
+    client_options.default_database = Some("auth".to_string());
+    client_options.app_name = Some("My App".to_string());
+
+    // Get a handle to the deployment.
+    let client = mongodb::Client::with_options(client_options)?;
+
+    let greeter = AuthService {
+        client: Arc::new(client),
+    };
 
     Server::builder()
-        .add_service(AuthServer::new(greeter))
+        .accept_http1(true)
+        .add_service(config.enable(AuthServer::new(greeter)))
         .serve(addr)
         .await?;
 
+    println!("Running on {}", addr);
     Ok(())
 }
