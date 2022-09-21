1de106609085c75569662e2fb59c06b6c6e396bd
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2022-09-21 15:22:03 +0200

    git modules
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
-fn main() {
-    tonic_build::compile_protos("proto/auth.proto")
+use failure::Error;
+
+fn main() -> Result<(), Error> {
+    tonic_build::configure()
+        .build_server(true)
+        .compile(&["proto/auth.proto"], &["googleapis", "grpc", "proto"])
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+
+    Ok(())
 }
